e45eba507d90b23d44238fecfe75c1e2350d98db
Paul C <hyp3rsp8c3@gmail.com>
2018-06-14 15:55:25 +0000

    added browser support fallbacks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ require("./gulp/tasks/styles");
 require("./gulp/tasks/watch");
 require("./gulp/tasks/sprites");
 require("./gulp/tasks/scripts");
+require("./gulp/tasks/modernizr");
 
 // gulp.task("default", function () {
 //     console.log("Hooray - you created a Gulp task");
